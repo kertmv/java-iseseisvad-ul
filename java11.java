ddd07edebcc58a656c1338da8f93761795543604
kertmv <115462783+kertmv@users.noreply.github.com>
2023-10-13 12:48:17 +0300

    Update java11.java

--- java11.java
+++ java11.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class java11 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 66; i++) {
-            if (i % 3 == 0) {
-                System.out.print(i + ", ");
+    public static void main
+    (String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("See programm annab sulle 1kuni66 arvud ja nad koik jagunevad 3-ka");
+            System.out.println("vajuta enterit palun.");
+            scanner.nextLine();
+
+            for (int i = 1; i <= 66; i++) {
+                if (i % 3 == 0) {
+                    System.out.print(i);
+                    if (i < 66 - 2) {
+                        System.out.print(", ");
+                    }
+                }
             }
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println("Viga: " + e.getMessage());
         }
     }
 }
