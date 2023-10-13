646a1f2a3c1f32a6d42eafce96241b88c4e3ea36
kertmv <115462783+kertmv@users.noreply.github.com>
2023-10-13 12:55:01 +0300

    Update java22.java

--- java22.java
+++ java22.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 public class java22 {
 
     public static void main(String[] args) {
-        // Väljastame pealkirja
+    
         System.out.println("nimi\tvanus\tsugu");
         
-        // Väljastame andmed
-        System.out.println("Jüri\t17\tm");
-        System.out.println("Malle\t17\tn");
-        System.out.println("Krõõt\t16\tn");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Sisesta oma nimi: ");
+            String nimi = scanner.nextLine();
+            System.out.println(nimi + "\t17\tm");
+
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Viga sisendi lugemisel: " + e.getMessage());
+        }
     }
 }
